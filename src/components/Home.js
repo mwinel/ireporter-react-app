e6a016ef200a23c476626c8faa94388b06cba353
Nelson Mwirumubi <mwinel@live.com>
2019-06-26 13:33:17 +0300

    Add heroku settings

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../styles/App.css';
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to iReporter!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
